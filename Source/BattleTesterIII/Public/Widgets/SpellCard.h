72d788e6b7f2006c374934914826385906eb2bfc
Bruno Girome <brunogirome@gmail.com>
2024-01-16 16:06:30 -0300

    Created a widget and render it at screen

---
+++ Source/BattleTesterIII/Public/Widgets/SpellCard.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SpellCard.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETESTERIII_API USpellCard : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
